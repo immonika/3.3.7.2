ec572221b6100d73f6ad3054143e0b7ba610c15b
immonika <36789796+immonika@users.noreply.github.com>
2021-07-16 05:10:53 +0530

    database

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,25 @@ from flask import Flask, render_template, send_from_directory, request, flash
 from flask_wtf import Form
 from wtforms import TextField, BooleanField, TextAreaField, SubmitField, validators, ValidationError
 from dotenv import load_dotenv
+import os
+# from flask import Flask, request, render_template
+from . import db
 
-load_dotenv()
 app = Flask(__name__)
-app.secret_key = 'development identification key'
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
+# app = Flask(__name__)
+# app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+# load_dotenv()
+# # app = Flask(__name__)
+# app.secret_key = 'development identification key'
+
+
+
+
+
+## Rest of the file
 @app.route('/')
 def index():
     return render_template('home.html', title="MLH Fellow", url=os.getenv("URL"))
@@ -42,3 +56,69 @@ class ContactForm(Form):
   subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
   message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
   submit = SubmitField("Send")
+
+#...
+from werkzeug.security import generate_password_hash
+from app.db import get_db
+
+#...
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
+
+#...
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.db import get_db
+
+#...
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
